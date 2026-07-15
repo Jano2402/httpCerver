89bc09e898732ec30b0605527847eea96fb45b91
Jano2402 <martinezalejandrouy7@gmail.com>
2026-07-15 20:51:09 -0300

    Defining the Client type

--- src/server.h
+++ src/server.h
@@ -6,6 +6,15 @@ typedef struct Server {
   int socket_fd;
 } Server;
 
+typedef struct Client {
+  int fd;
+  char ip[INET_ADDRSTRLEN];
+  uint16_t port;
+  uint64_t bytes_sent;
+  uint64_t bytes_received;
+  time_t connected_at;
+} Client;
+
 int server_init(Server *server, int port);
 int server_start(Server *server);
 void server_destroy(Server *server);
