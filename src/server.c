ec66746c9ae1e0c925d394a7a1c2f2281aab27c8
Jano2402 <martinezalejandrouy7@gmail.com>
2026-08-06 21:16:04 -0300

    Now the server supports multiple connections at the same time.

--- src/server.c
+++ src/server.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -28,10 +30,42 @@ int server_init(Server *server, int port)
   return 0;
 }
 
+void* atender_cliente(void* arg)
+{
+  int client_socket = *(int *)arg;
+  free(arg);
+
+  char buffer[256];
+
+  while(1) 
+  {
+    int bytes_recieved = recv(client_socket, buffer, sizeof(buffer), 0);
+
+    if(bytes_recieved < 0)
+    {
+      perror("recv");
+      break;
+    }
+
+    if(bytes_recieved == 0)
+    {
+      printf("Client disconnected\n");
+      break;
+    }
+
+    buffer[bytes_recieved] = '\0';
+    printf("Recieved: %s\n", buffer);
+  }
+  
+  close(client_socket);
+  return NULL;
+}
+
 int server_start(Server *server)
 {
   struct sockaddr_in addr, peer_addr;
-  char buffer[256];
+  // char buffer[256];
+  // char ip[INET6_ADDRSTRLEN];
 
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
@@ -55,50 +89,23 @@ int server_start(Server *server)
   socklen_t peer_addr_size = sizeof(peer_addr);
   memset(&peer_addr, 0, peer_addr_size);
 
-  int client_fd = accept(server->socket_fd, (struct sockaddr *) &peer_addr, &peer_addr_size);
-  if (client_fd == -1)
-  {
-    perror("accept");
-    return -1;
-  }
-
-  printf("Client connected on file descriptor: %d\n", client_fd);
-  printf("The connection entered from: ");
-  char ip[INET6_ADDRSTRLEN];
-  struct sockaddr_in *s = (struct sockaddr_in *) &peer_addr;
-  inet_ntop(AF_INET, &s->sin_addr, ip, sizeof(ip));
-  printf("%s:%d\n", ip, ntohs(s->sin_port));
-  printf("Family: %s\n", peer_addr.sin_family == AF_INET ? "IPv4" : "IPv6");
-
-  strcpy(buffer, "This message was sent from the server \n");
-  if (send(client_fd, buffer, strlen(buffer), 0) == -1)
-  {
-    perror("send");
-    return -1;
-  }
-
   while (1)
   {
-    ssize_t n = recv(client_fd, buffer, sizeof(buffer), 0);
-    if(n == -1)
+    int client_fd = accept(server->socket_fd, (struct sockaddr *) &peer_addr, &peer_addr_size);
+    if (client_fd == -1)
     {
-      perror("recv");
-      return -1;
-    } else if (n == 0) {
-      printf("The client closed the connection\n");
-      return 0;
+        perror("accept");
+        continue;
     }
-    buffer[n] = '\0';
-    printf("Client said: %s\n", buffer);
-    if (send(client_fd, buffer, strlen(buffer), 0) == -1)
-    {
-      perror("send");
-      return -1;
-    }
+
+    int *socket = malloc(sizeof(int));
+    *socket = client_fd;
+    
+    pthread_t thread;
+
+    pthread_create(&thread, NULL, atender_cliente, socket);
+    pthread_detach(thread);
   }
-
-  // Next step could be handling multiple clients at the same time.
-
   return 0;
 }
 
